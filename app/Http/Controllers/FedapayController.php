9377d759eaf9fd6936d912bc704fe3511190e64c
Julian-root <adjibiadechinan@gmail.com>
2023-12-27 00:00:05 +0100

    part 6

--- app/Http/Controllers/FedapayController.php
+++ app/Http/Controllers/FedapayController.php
@@ -35,11 +35,11 @@ class FedapayController extends Controller
         // FedaPay\FedaPay::setEnvironment("sandbox");
         // FedaPay\FedaPay::setApiKey("sk_sandbox_-KGmRqvYTOJiNyCi2atfDuvF");
 
-        FedaPay\FedaPay::setEnvironment("live");
-        FedaPay\FedaPay::setApiKey("sk_live_Y1qYCqcrx0JJRLMw5M9Aj8Jo");
-
         // FedaPay\FedaPay::setEnvironment("live");
-        // FedaPay\FedaPay::setApiKey("sk_live_R2ageX5UpB28xXGifeUGtNRD");
+        // FedaPay\FedaPay::setApiKey("sk_live_Y1qYCqcrx0JJRLMw5M9Aj8Jo");
+
+        FedaPay\FedaPay::setEnvironment("live");
+        FedaPay\FedaPay::setApiKey("sk_live_R2ageX5UpB28xXGifeUGtNRD");
 
 
         try {
