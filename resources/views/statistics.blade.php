3020c5cb4680cf3cb39f1d047eb6e36789fc42db
Julian-root <adjibiadechinan@gmail.com>
2023-12-27 00:55:26 +0100

    part 6

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -6,6 +6,9 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Table 06</title>
 
+    <link rel="apple-touch-icon" href="assets/images/fav.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/logo.png">
+
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href={{asset('stat/css')}} rel="stylesheet" type="text/css">
     <link rel="stylesheet" href={{asset('stat/font-awesome.min.css')}}>
